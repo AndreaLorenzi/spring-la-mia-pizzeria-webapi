c47dee342fc5c1ffeef915a275725c3c50487bef
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-11-10 16:08:09 +0100

    friday exercise complete

--- src/main/java/com/experis/course/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/controller/PizzaController.java
@@ -4,12 +4,17 @@ import com.experis.course.springlamiapizzeriacrud.model.Pizza;
 import com.experis.course.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -19,9 +24,33 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model) {
-        List<Pizza> pizzaList = pizzaRepository.findAll();
+    public String index(@RequestParam Optional<String> search, Model model) {
+        List<Pizza> pizzaList;
+
+        if (search.isPresent()) {
+            // se il parametro di ricerca è presente filtro la lista dei libri
+            pizzaList = pizzaRepository.findByNameContainingIgnoreCase(search.get());
+        } else {
+            // altrimenti prendo tutti i libri non filtrati
+            // bookRepository recupera da database la lista di tutti i libri
+            pizzaList = pizzaRepository.findAll();
+        }
         model.addAttribute("pizzaList", pizzaList);
         return "pizzas/home";
+    }
+
+    // metodo che mostra i dettagli di un libro preso per id
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        // verifico se il risultato è presente
+        if (result.isPresent()) {
+            // passo al template l'oggetto Book
+            model.addAttribute("pizza", result.get());
+            return "pizzas/show";
+        } else {
+            // se non ho trovato il libro sollevo un'eccezione
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "pizza with id " + id + " not found");
+        }
     }
 }
