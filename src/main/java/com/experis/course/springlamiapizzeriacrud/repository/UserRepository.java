c668e3b78879f6cc0f2a30d57d7b7290982db6ba
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-11-20 14:18:49 +0100

    add user repository

--- src/main/java/com/experis/course/springlamiapizzeriacrud/repository/UserRepository.java
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.experis.course.springlamiapizzeriacrud.repository;
 
-public interface UserRepository {
+import com.experis.course.springlamiapizzeriacrud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
